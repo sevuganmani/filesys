5d1de7457e003af8627580190248ac0cddf2c9ad
root <sevugan96mani@gmail.com>
2021-02-14 15:12:12 +0000

    updated filecheck

--- filecheck.rb
+++ filecheck.rb
@@ -1,6 +1,6 @@
-#
-if(File.exist?('file.rb')) 
-  puts 'file or directory exists'
+#!/usr/bin/ruby
+if(File.exist?('/vagrant/chef-repo/cookbooks/qasystem/recipes/filesys/file.rb')) 
+  puts 'file.rb exists'
 else 
-  puts 'file or directory not found'
+  puts 'file.rb does not exists'
 end
